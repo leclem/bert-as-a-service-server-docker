3778afdefd6b19b7ef8bf5dc4647a4bfcba5ba83
Clement Moutet <clementmoutet@gmail.com>
2020-09-29 20:04:03 +0200

    change sentences

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from bert_serving.client import BertClient
 print("eee")
 bc = BertClient("172.17.0.2")
-arrayResults = bc.encode(['Tais toi sale merde', 'Je fais la cuisine'])
+arrayResults = bc.encode(['I am doing something', 'I am not doing it'])
 
 from scipy import spatial
 print(1-spatial.distance.cosine(arrayResults[0], arrayResults[1]))
